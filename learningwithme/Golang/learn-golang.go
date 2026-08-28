1a0545b7e06058e83a03a1c1a444e2c95b11a92e
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 23:48:41 +0700

    Create learn-golang.go

---
+++ learningwithme/Golang/learn-golang.go
@@ -0,0 +1,17 @@
+## Tất cả các hàm và biến của Golang
+
+# 1. BIẾN (Variables) trong Go
+var = Variable
+:= = Short Variable Declaration 
+const = Constant
+_ = Blank Identifier
+iota = Iota
+
+# 2. HÀM (Functions) trong Go
+func = Function
+return = Return
+defer = Deferred Execution
+go = Goroutine
+chan = Channel
+(r Receiver) = Method Receiver
+
