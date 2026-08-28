41d9c787239982425913bfbd28fb0f50257edce9
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 11:04:42 +0700

    Update learn.py

--- learningwithme/learn.py
+++ learningwithme/learn.py
@@ -1,4 +1,11 @@
+### tên biến và hàm viết tắt và đầy đủ trong ngôn ngữ lập trình python ###
+
 var = variable
 int = integer
 float = float-point number
 complex = complex number
+str = string
+list = list 
+tuple = tuple
+dict = dictionary
+set = set
