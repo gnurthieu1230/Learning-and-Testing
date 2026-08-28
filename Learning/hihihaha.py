b67f2604cd0e78103636c217053b322a28085d78
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 10:42:30 +0700

    Create hihihaha.py

---
+++ Learning/hihihaha.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+def manager (__init__main):
+   path = "local.filesystem"
+   file = "system.manager (path.dir)"
+   os = "system.process"
+   local = "os.path"
+   root = "check.filesystem.path"
+   directory = "storage.process.filesys"
+   task = "manager.file" 
+print("path, file, os, local,root, directory.")
+
+  if path_error
+path_error = 1
+   else:
+path_error = "error.directory"
+
+ ==> path.error = directory.false
+
+  if process.crash
+process = 1 
+  else:
+process.crash = false
+  
+  
+
+    
