f872884cb45e260ca29761303cd59332612d1b34
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 14:37:49 +0700

    Update learn-rust.rs

--- learningwithme/Rust/learn-rust.rs
+++ learningwithme/Rust/learn-rust.rs
@@ -5,3 +5,12 @@
 let = Let
 mut = Mutable
 const = Constant
+
+# 2. Các kiểu dữ liệu cơ bản (Primitive Data Types)
+i8 .. i128 = Signed Integer
+u8 .. u128 = Unsigned Integer
+isize / usize = Pointer-sized Integer
+f32 / f64 = Floating-point
+bool = Boolean
+char = Character
+
