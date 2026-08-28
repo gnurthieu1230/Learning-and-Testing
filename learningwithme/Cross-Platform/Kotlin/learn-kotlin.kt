92b60b00dc344f9a39d6411dd59f2d5291b3cfbc
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 23:23:25 +0700

    Update learn-kotlin.kt

--- learningwithme/Cross-Platform/Kotlin/learn-kotlin.kt
+++ learningwithme/Cross-Platform/Kotlin/learn-kotlin.kt
@@ -1,7 +1,18 @@
 ## Tất cả hàm và biến của Kotlin ###
+
+# 1. Từ khóa về BIẾN (Variables)
 val = Value
 var = Variable
 const val = Constant Value
 lateinit var = Late Initialization Variable
 ? = Nullable
 ?: = Elvis Operator
+
+# 2. Từ khóa về HÀM (Functions)
+fun = function
+Unit = Unit
+return = Return
+= = Expression
+infix = Infix Notation
+inline = Inline Function
+suspend = Suspend
