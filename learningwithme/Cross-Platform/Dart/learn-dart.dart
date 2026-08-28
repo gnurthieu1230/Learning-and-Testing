87ec3aa805c1d2df0f5e6616dbf479c894aa6099
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 23:11:27 +0700

    Update learn-dart.dart

--- learningwithme/Cross-Platform/Dart/learn-dart.dart
+++ learningwithme/Cross-Platform/Dart/learn-dart.dart
@@ -14,4 +14,13 @@ bool = Boolean
 arr = Array
 obj = Object
 
-
+# 2. từ khóa & Viết tắt về HÀM trong Dart
+void = Void
+=> = Fat Arrow
+async = Asynchronous
+await = Await
+Future = Future
+Stream = Stream
+required = Required
+return = Return
+typedef = Type Definition
