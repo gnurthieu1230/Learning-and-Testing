3c74959e6fcdaad8ac22a6cf77e36c0513042c04
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 13:00:05 +0700

    Add files via upload

---
+++ learningwithme/Python/learn-python.py
@@ -0,0 +1,52 @@
+### TÊN VIẾT TẮT VÀ ĐẦY ĐỦ TRONG PYTHON ###
+
+# 1. Biến & Từ khóa (Variables & Keywords)
+var = variable
+def = define
+elif = else if
+
+# 2. Kiểu dữ liệu (Data Types)
+int = integer
+float = floating-point number
+complex = complex number
+str = string
+bool = boolean
+list = list
+tuple = tuple
+dict = dictionary
+set = set
+frozenset = frozen set
+bytes = bytes
+bytearray = byte array
+memoryview = memory view
+NoneType = none type
+
+# 3. Hàm toán học & Xử lý cơ bản
+abs() = absolute
+len() = length
+max() / min() = maximum / minimum
+pow() = power
+divmod() = division & modulus
+ord() = ordinal
+chr() = character
+ascii() = ASCII
+repr() = representation
+
+# 4. Hàm kiểm tra & Thao tác trên đối tượng
+isinstance() = is instance
+issubclass() = is subclass
+getattr() = get attribute
+setattr() = set attribute
+delattr() = del attribute
+hasattr() = has attribute
+
+# 5. Hàm chuyển đổi hệ cơ số
+bin() = binary
+hex() = hexadecimal
+oct() = octal
+
+# 6. Hàm lặp & Thực thi
+iter() = iterator
+next() = next
+eval() = evaluate
+exec() = execute
