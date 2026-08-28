7bcd632d1b73cac6106b7fc0ce2c4ceb02722484
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 11:07:27 +0700

    Update learn.py

--- learningwithme/learn.py
+++ learningwithme/learn.py
@@ -2,10 +2,16 @@
 
 var = variable
 int = integer
-float = float-point number
+float = floating-point number
 complex = complex number
 str = string
 list = list 
 tuple = tuple
 dict = dictionary
 set = set
+bool = boolean
+frozenset = frozen set
+bytes = bytes
+bytearrays = bytes arrays
+memoryview = memory view
+nonetype = nonetype
