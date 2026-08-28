0fe735fc8b0c98b746e95517274e6a3466825384
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 23:41:57 +0700

    Create learn-swift.swift

---
+++ learningwithme/Cross-Platform/Swift/learn-swift.swift
@@ -0,0 +1,25 @@
+### Tất cả hàm và biến của Swift ###
+
+# 1. BIẾN (Variables) trong Swift
+let = Let (let x be)
+var = Varunable
+nil = Nil (Nothing in law)
+? = Optional
+! = Force Unwrapping
+if let = Optional Binding (if)
+guard let = Optional Binding (Guard)
+lazy var = Lazy Initialization Variable
+
+# 2. HÀM (Functions) trong Swift
+func = Function
+-> = Return Arrow
+void = Void
+_ = Wildcard / Omitter
+inout = In-Out Parameter
+async = Asynchronous
+await = Await
+throws = Throws Error
+try = Try
+closure = Closure
+
+
