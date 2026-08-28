b6fe30e2a918c60b9e8d700fe5b52aff789eb2e6
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 23:06:36 +0700

    Create learn-dart.dart

---
+++ learningwithme/Cross-Platform/Dart/learn-dart.dart
@@ -0,0 +1,17 @@
+## Tất cả các hàm với biến của Dart
+
+# 1. BIẾN (Variable & Data Types)
+var = Variable
+let = Let (Let x be equal to...)
+const = Constant
+val = Value
+int = Integer
+float = Floating-point number
+double = Double-precision floating-point
+str / String = String
+char = Character
+bool = Boolean
+arr = Array
+obj = Object
+
+
