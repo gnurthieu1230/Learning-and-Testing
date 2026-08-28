1fc6a9d0ae1f33bc8783dd42fca493d244f8139f
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-28 13:47:50 +0700

    Update learn-cpp.cpp

--- learningwithme/C++/learn-cpp.cpp
+++ learningwithme/C++/learn-cpp.cpp
@@ -1,1 +1,9 @@
-.
+### Các kí tự viết tắt hàm và biến trong C++ ###
+
+# 1. Các kiểu dữ liệu cơ bản (Basic Data Types)
+int = Integer
+float = floating-point
+double = double precision
+char = Character
+bool = boolean
+void = ...
