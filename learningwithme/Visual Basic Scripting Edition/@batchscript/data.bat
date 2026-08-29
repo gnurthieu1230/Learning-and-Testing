8fae2ab87b781949f84e5f7ab5ad4d03c5439c39
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-29 23:47:17 +0700

    Create data.bat

---
+++ learningwithme/Visual Basic Scripting Edition/@batchscript/data.bat
@@ -0,0 +1,8 @@
+@echo off
+color 0a
+title HACKING IN PROGRESS...
+echo DANG KET NOI VOI MAY CHU...
+timeout /t 2 >nul
+:loop
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+goto loop
