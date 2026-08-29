81cbe756255dce9856b7bc91a2d4686648e017ca
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-29 10:19:03 +0700

    Update learn-cli.py

--- learningwithme/Termux + Terminal/learn-cli.py
+++ learningwithme/Termux + Terminal/learn-cli.py
@@ -8,4 +8,9 @@ pkg uninstall [tên_gói] = gỡ cài đặt một gói phần mềm
 pkg search [tên_gói] = Tìm kiếm xem có phần mềm nào trên termux không
 pkg list-installed = Hiện thị danh sách các gói đã cài đặt
 
-2. 
+2. Thao tác với Tệp và Thư mục (File & Directory)
+pwd = Hiện thị đường dẫn thư mục đang đứng
+ls = liệt kê các thư mục/file hiện tại
+ls -a = Liệt kê tất cả file (bao gồm cả file ẩn có dấu "." ở đầu)
+ls -l = Hiển thị danh sách kèm thông tin chỉ tiết (dung lượng, quyền, ngày tạo)
+cd [đường_dẫn] = Di chuyển đến thư mục cần mở
