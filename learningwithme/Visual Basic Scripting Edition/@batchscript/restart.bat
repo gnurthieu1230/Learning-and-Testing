049f7d028c33589b2f02216656c87bda0c85be9c
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-29 23:53:23 +0700

    Create restart.bat

---
+++ learningwithme/Visual Basic Scripting Edition/@batchscript/restart.bat
@@ -0,0 +1,5 @@
+@echo off
+title THONG BAO CAP NHAT WINDOWS
+echo He thong phat hien ban dang su dung qua lau...
+echo May tinh se tu dong RESTART sau 30 giay de giai phong RAM!
+shutdown -r -t 30 -c "Dang cai dat ban cap nhat Windows 12 gia lap, vui long khong tat may!"
