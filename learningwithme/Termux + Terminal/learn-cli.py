8912bfc1bc46fc7e7003c0f7255cc35104437ad7
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-29 10:14:32 +0700

    Create learn-cli.py

---
+++ learningwithme/Termux + Terminal/learn-cli.py
@@ -0,0 +1,11 @@
+### Tất cả các lệnh trong Termux (Android), Terminal (Windows, Linux, MacOS), Powershell (Windows)
+
+=> Termux
+1. Quản lý gói phần mềm (APT / PKG)
+pkg update && pkg upgrade = Cập nhật và nâng cấp toàn bộ gói cài đặt
+pkg install [tên_gói] = Cài đặt một phần mềm mới
+pkg uninstall [tên_gói] = gỡ cài đặt một gói phần mềm 
+pkg search [tên_gói] = Tìm kiếm xem có phần mềm nào trên termux không
+pkg list-installed = Hiện thị danh sách các gói đã cài đặt
+
+2. 
