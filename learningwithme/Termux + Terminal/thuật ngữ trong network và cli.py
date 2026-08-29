db1eae6f3ff288d51aee3bc9a68d106a594e874c
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-29 18:46:42 +0700

    Create thuật ngữ trong network và cli.py

---
+++ learningwithme/Termux + Terminal/thuật ngữ trong network và cli.py
@@ -0,0 +1,29 @@
+### Tất cả các thuật ngữ của mạng (network) và môi trường dòng lệnh (CLI)
+
+---------------------------------------
+    THUẬT NGỮ TRONG NETWORK VÀ CLI
+          ------------------
+1. WAN = Wide Area Network
+2. LAN = Local Area Network
+3. TCP = Transmission Control Protocol
+4. UDP = User Datagram Protocol
+5. IP = Internet Protocol
+6. UPnP = Universal Plug and Play
+7. NAT = Network Address Translation
+8. CGNAT = Carrier-Grade NAT
+9. IPv4 = Internet Protocol version 4
+10. IPv6 = Internet Protocol version 6
+11. DNS = Domain Name Server
+12. BSSID = Basic Service Set Identifier
+13. SSID = Service Set Identifier
+14. RSSI = Received Signal Strength Indicator
+15. VPN = Virtual Private Network
+16. APN = Access Point Name
+17. WPS = Wi-Fi Protected Setup
+18. WPA = Wi-Fi Protected Access
+19. WPA2 = Wi-Fi Protected Access 2
+20. WPA3 = Wi-Fi Protected Access 3
+21. TKIP = Temporal Key Integrity Protocol
+22. AES = Advanced Encryption Standard
+23. MAC = Media Access Control Address
+---------------------------------------
