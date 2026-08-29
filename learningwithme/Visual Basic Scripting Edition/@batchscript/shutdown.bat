376983af6d6ec80929e0943303eae6898139d54c
Nguyễn Trung Hiếu <gnurthieu1230@gmail.com>
2026-08-29 23:51:22 +0700

    Create shutdown.bat

---
+++ learningwithme/Visual Basic Scripting Edition/@batchscript/shutdown.bat
@@ -0,0 +1,5 @@
+@echo off
+title THONG BAO HE THONG
+echo Máy tính của bạn đã bị nhiễm độc!
+echo He thong se tu dong SHUTDOWN sau 60 giay...
+shutdown -s -t 60 -c "Virus dang xoa o C:, chuc ban may man lan sau!"
